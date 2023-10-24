e62b5908f7dac2d5ffa59cd163c8f7d83757743e
babyblue <lijl@blackbeanssoft.com>
2023-10-24 16:19:49 +0800

    fix basic style

--- src/utils/date-helper.ts
+++ src/utils/date-helper.ts
@@ -1,4 +1,17 @@
-export const displayDate = (date: Date) => {
+// 格式化日期 2023/10/24 07:43:40
+const getFormatDate = (date: Date) => {
   const localDate = date.getTime() + date.getTimezoneOffset() * 60 * 1000
-  return new Date(localDate).toISOString().slice(0, 11).replace('T', '')
+  return new Date(localDate).toISOString().slice(0, 19).replace('T', ' ').replaceAll('-', '/')
 }
+
+// 转换YMD日期格式 2022/10/10
+export const formatedYMD = (date: Date) => getFormatDate(date).split(' ')[0]
+
+// 时间格式 07:43:40
+export const formatedTime = (date: Date) => getFormatDate(date).split(' ')[1]
+
+// 年份 2023
+export const getYear = (date: Date) => formatedYMD(date).split('/')[0]
+
+// 日月 10/24
+export const getMD = (date: Date) => formatedYMD(date).split('/').slice(1, 3).join('/')
