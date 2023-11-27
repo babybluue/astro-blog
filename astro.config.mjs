c9815a29ba8f5ad05d83bd4d40fc7c7ebfc7dd65
babyblue <lijl@blackbeanssoft.com>
2023-11-27 22:32:13 +0800

    fix page

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ export default defineConfig({
       registerType: 'prompt',
       devOptions: { enabled: true },
       workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,svg,png,txt,webp}'],
+        globPatterns: ['**/*.{js,css,html,ico,svg,png,txt,ttf,webp}'],
       },
       includeAssets: ['favicon.ico', 'pwa_192.png'],
       manifest: {
--- src/utils/post-link.ts
+++
@@ -1,3 +0,0 @@
-import type { PostModel } from '@interfaces/post-model'
-
-export const postLink = (post: PostModel) => `/post/${post.data.title}`
