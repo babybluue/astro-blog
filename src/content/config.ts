5b51773cd4ecb7534666f34f95201c502a5d9bae
babyblue <lijl@blackbeanssoft.com>
2024-04-19 00:50:32 +0800

    update content

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,28 +2,32 @@ import { defineCollection, z } from 'astro:content'
 
 const posts = defineCollection({
   type: 'content',
-  schema: z.object({
-    isDraft: z.boolean().optional(),
-    language: z.enum(['zh', 'en']).optional(),
-    title: z.string(),
-    date: z.date(),
-    abbrlink: z.string(),
-    tags: z.array(z.string()),
-    description: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      isDraft: z.boolean().optional(),
+      language: z.enum(['zh', 'en']).optional(),
+      title: z.string(),
+      date: z.date(),
+      abbrlink: z.string(),
+      tags: z.array(z.string()),
+      description: z.string().optional(),
+      cover: image().optional(),
+    }),
 })
 
 const notes = defineCollection({
   type: 'content',
-  schema: z.object({
-    isDraft: z.boolean().optional(),
-    language: z.enum(['zh', 'en']).optional(),
-    title: z.string(),
-    date: z.date(),
-    abbrlink: z.string(),
-    tags: z.array(z.string()),
-    description: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      isDraft: z.boolean().optional(),
+      language: z.enum(['zh', 'en']).optional(),
+      title: z.string(),
+      date: z.date(),
+      abbrlink: z.string(),
+      tags: z.array(z.string()),
+      description: z.string().optional(),
+      cover: image().optional(),
+    }),
 })
 
 export const collections = { posts, notes }
