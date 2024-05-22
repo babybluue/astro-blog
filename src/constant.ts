380699c25e674b88770feeb9afc849f2c00e33d4
babyblue <lijl@blackbeanssoft.com>
2024-05-22 10:45:54 +0800

    fix pagination and add constant file

---
+++ src/constant.ts
@@ -0,0 +1,12 @@
+import avatarImg from '@assets/avatar.jpg'
+
+// index
+
+export const title = 'didmax'
+
+export const description = '这是一个托管在 Cloudflare 上的个人博客，用于记录生活和技术笔记。'
+
+export const avatar = avatarImg
+
+export const quote =
+  '我个人的一个秘密是在需要极大快乐和悲伤的公众场合却达不到这种快乐和悲伤应有的水平，因而内心惊恐万状，汗如雨下'
