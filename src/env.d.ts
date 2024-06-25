6e356a5950a00e174d85bedadc7c08f7ff97304f
babyblue <lijl@blackbeanssoft.com>
2024-06-25 16:39:59 +0800

    add pagefind

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,9 @@
 /// <reference types="astro/client" />
 /// <reference types="vite-plugin-pwa/client" />
 /// <reference types="vite-plugin-pwa/info" />
+
+declare module '@pagefind/default-ui' {
+  declare class PagefindUI {
+    constructor(arg: any)
+  }
+}
