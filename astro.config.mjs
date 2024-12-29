4d6560d6894dfca2f0bd16748f73551e277c14cc
didmax <jialongli666@gmail.com>
2024-12-30 00:16:00 +0800

    fix ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,18 @@
-import { globalVars, site } from './constant';
-import AstroPWA from '@vite-pwa/astro';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import { defineConfig } from 'astro/config';
+import { globalVars, site } from './constant'
+import AstroPWA from '@vite-pwa/astro'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
-
-
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-import rehypeExternalLinks from 'rehype-external-links';
-import rehypeSlug from 'rehype-slug';
-import rehypeToc from 'rehype-toc';
-import remarkRehype from 'remark-rehype';
-import { transformerNotationDiff, transformerNotationHighlight } from '@shikijs/transformers';
-
+import rehypeAutolinkHeadings from 'rehype-autolink-headings'
+import rehypeExternalLinks from 'rehype-external-links'
+import rehypeSlug from 'rehype-slug'
+import rehypeToc from 'rehype-toc'
+import { remarkAlert } from 'remark-github-blockquote-alert'
+import remarkRehype from 'remark-rehype'
+import sectionize from 'remark-sectionize'
+import { transformerNotationDiff, transformerNotationHighlight } from '@shikijs/transformers'
 
 const globalVarsString = Object.entries(globalVars)
   .map(([key, value]) => `@${key}: ${value};`)
@@ -26,11 +25,12 @@ export default defineConfig({
     prefetchAll: true,
   },
   markdown: {
+    remarkRehype: { footnoteLabelTagName: 'h3', footnoteLabel: ' ' },
     shikiConfig: {
       theme: 'one-dark-pro',
       transformers: [transformerNotationHighlight(), transformerNotationDiff()],
     },
-    remarkPlugins: [remarkRehype],
+    remarkPlugins: [sectionize, remarkAlert, remarkRehype],
     rehypePlugins: [
       rehypeSlug,
       [rehypeAutolinkHeadings, { behavior: 'after' }],
